c83243828a13cc0aaa65ee8eb13a95a5b08cf486
Henrique Costa <henrique.costa@atlastechnol.com>
2022-12-31 16:48:47 -0300

    initial commit, insert project base

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="classe of classes.data">
+        <p> {{ classe.name }}</p>
+        <img :src="classe.image" :alt="classe.name">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data (){
+    return {
+      classes: []
+    }
+  },
+
+  created() {
+    let promise = this.$http.get('https://eldenring.fanapis.com/api/classes');
+    promise.then(res => res.json().then(classes => this.classes = classes));
+  }
+}
+</script>
+
+<style>
+
+</style>
