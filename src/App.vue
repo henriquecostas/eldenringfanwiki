40368d33fee7c4525a9f067cd91981e4c8a21f3c
Henrique Costa <henrique.costa@atlastechnol.com>
2023-01-17 17:52:27 -0300

    feat: insert filter by class name

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
+  <div class="app">
     <div class="class-info-panel">
       <ul class="class-info-list">
-        <li class="class-info-list-item" v-for="classe of classes.data">
+        <li class="class-info-list-item" v-for="classe of filteredClasses">
           <class-panel 
             :className="classe.name" 
             :classTitle="classe.name"
@@ -14,7 +14,16 @@
         </li>
       </ul>
     </div>
-</div>
+    <div class="filter-container">
+      <input
+        class="filter" 
+        type="search" 
+        name="filter-list"
+        placeholder="Type here to filter by class name"
+        v-on:input="filterItem = $event.target.value" 
+      />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -27,14 +36,28 @@ export default {
 
   data (){
     return {
-      classes: []
+      classes: [],
+      filterItem: ''
     }
   },
 
   created() {
     this.$http.get('https://eldenring.fanapis.com/api/classes')
     .then(res => res.json())
-    .then(classes => this.classes = classes, err => console.log(err));
+    .then(classes => this.classes = classes.data, err => console.log(err));
+  },
+
+  computed: {
+    filteredClasses() {
+
+      if (this.filterItem) {
+        let exp = new RegExp(this.filterItem.trim(), 'i');
+        return this.classes.filter(classe => exp.test(classe.name));
+      } else {
+        return this.classes
+      }
+    }
+
   }
 }
 </script>
@@ -70,6 +93,44 @@ export default {
   
   .class-info-list-item{
     display: table-cell;
+  }
+
+  /* ===== Scrollbar CSS ===== */
+  /* Firefox */
+  * {
+    scrollbar-width: auto;
+    scrollbar-color: #a79d6c #393432;
+  }
+
+  /* Chrome, Edge, and Safari */
+  *::-webkit-scrollbar {
+    width: 4px;
+  }
+
+  *::-webkit-scrollbar-track {
+    background: #393432;
+  }
+
+  *::-webkit-scrollbar-thumb {
+    background-color: #a79d6c63;
+    border-radius: 10px;
+    border: 1px solid #585446;
+  }
+
+  .filter{
+    width: 400px;
+    margin-top: 20px;
+    border: 1px solid rgb(36 35 31 / 90%);
+    background-color: rgb(36 35 31 / 70%);
+    color: #DCDCDC;
+    border-radius: 8px;
+    padding-left: 8px;
+  }
+
+  .filter-container{
+    width: 50vh;
+    margin: 0 auto;
+    margin-top: 8px;
   }
 
 </style>
